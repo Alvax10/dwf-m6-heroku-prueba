6921d1acd618c1a60587d879acb9902ed7322c0c
alvaro <alvaro695547@gmail.com>
2021-10-04 18:00:22 -0300

    env vars

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
 import * as express from "express";
 const app = express();
 const port = process.env.PORT || 1234;
+// Para futuros usos
+// const dev = process.env.NODE_ENV == "development";
+
+console.log(process.env.NODE_ENV)
+console.log(process.env.DB_HOST)
+
+app.get("/env",(req, res)=> {
+    res.json({
+        environment: process.env.NODE_ENV,
+    });
+});
 
 app.get("/hola",(req, res)=> {
     res.json({
